47b25b300eb8704539bf44bb2816ab668ecb01a5
Arun <arun@minebrat.com>
2023-09-06 17:40:59 +0530

    updated with new version and style

--- src/ckeditor.js
+++ src/ckeditor.js
@@ -137,5 +137,54 @@ ClassicEditor.defaultConfig = {
 			'mergeTableCells',
 			'tableProperties'
 		]
+	},
+	style: {
+		definitions: [
+			{
+				name: 'Article category',
+				element: 'h3',
+				classes: [ 'category' ]
+			},
+			{
+				name: 'Title',
+				element: 'h2',
+				classes: [ 'document-title' ]
+			},
+			{
+				name: 'Subtitle',
+				element: 'h3',
+				classes: [ 'document-subtitle' ]
+			},
+			{
+				name: 'Info box',
+				element: 'p',
+				classes: [ 'info-box' ]
+			},
+			{
+				name: 'Side quote',
+				element: 'blockquote',
+				classes: [ 'side-quote' ]
+			},
+			{
+				name: 'Marker',
+				element: 'span',
+				classes: [ 'marker' ]
+			},
+			{
+				name: 'Spoiler',
+				element: 'span',
+				classes: [ 'spoiler' ]
+			},
+			{
+				name: 'Code (dark)',
+				element: 'pre',
+				classes: [ 'fancy-code', 'fancy-code-dark' ]
+			},
+			{
+				name: 'Code (bright)',
+				element: 'pre',
+				classes: [ 'fancy-code', 'fancy-code-bright' ]
+			}
+		]
 	}
 };
